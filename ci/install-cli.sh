430f046570b1c9cce2568dd7457a8e4ad745c14a
Rahul Jain <rahulkj@gmail.com>
2022-12-07 17:43:55 -0600

    un pinning to jammy

--- ci/install-cli.sh
+++ ci/install-cli.sh
@@ -37,13 +37,13 @@ install_docker() {
         ca-certificates \
         curl \
         gnupg \
-        lsb-release
+        lsb-release -y
 
     mkdir -p /etc/apt/keyrings
 
     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian jammy stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     apt-get update
 
