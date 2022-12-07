95ecc7f8caff41863be5480a5c20d3220407223e
Rahul Jain <rahulkj@gmail.com>
2022-12-07 17:34:30 -0600

    Fixing the docker install

--- ci/install-cli.sh
+++ ci/install-cli.sh
@@ -1,20 +1,57 @@
-#!/bin/bash -x
+#!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
 
 apt upgrade
 
-apt install docker jq -y
+install_jq() {
+    echo "installing jq"
+    apt install jq -y
+}
 
-curl -L https://carvel.dev/install.sh | K14SIO_INSTALL_BIN_DIR=/usr/local/bin/ bash
+install_carvel() {
+    echo "installing carvel tools"
+    curl -L https://carvel.dev/install.sh | K14SIO_INSTALL_BIN_DIR=/usr/local/bin bash
+}
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install_kubectl() {
+    echo "installing kubectl"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-chmod +x kubectl
-mv kubectl /usr/local/bin/
+    chmod +x kubectl
+    mv kubectl /usr/local/bin/
+}
 
-wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+install_yq() {
+    echo "installing yq"
+    wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 
-chmod a+x /usr/local/bin/yq
+    chmod a+x /usr/local/bin/yq
 
-yq --version
+    yq --version
+}
+
+install_docker() {
+    echo "installing docker"
+    apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+
+    mkdir -p /etc/apt/keyrings
+
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt-get update
+
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+}
+
+install_yq
+install_carvel
+install_kubectl
+install_yq
+install_docker
