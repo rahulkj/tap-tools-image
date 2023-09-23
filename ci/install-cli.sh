55c7f0c5b1a8e257834b5f62e6200498874a785a
Rahul Jain <rahulkj@gmail.com>
2023-09-23 16:03:28 -0500

    Adding logic to download kpack cli

--- ci/install-cli.sh
+++ ci/install-cli.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+export OUTPUT=/usr/local/bin
+
+REPO_YQ=mikefarah/yq
+REPO_KP=buildpacks-community/kpack-cli
 
 apt upgrade
 
+get_latest_release() {
+    DOWNLOAD_URL=$(curl -L --silent "https://api.github.com/repos/$1/releases/latest" | \
+      jq -r \
+      --arg flavor $2 '.assets[] | select(.name | contains($flavor)) | .browser_download_url')
+}
+
 install_jq() {
-    echo "installing jq"
-    apt install jq -y
+    log 'Installing yq'
+
+    get_latest_release "$REPO_YQ" "linux_amd64"
+
+    while read -r line; do
+      if [[ "$line" != *.tar.gz ]]; then
+        wget -qO "$OUTPUT"/yq "$line"
+        chmod +x "$OUTPUT"/yq
+      fi
+    done <<< "$DOWNLOAD_URL"
+
+    echo "yq cli:" $(yq --version)
 }
 
 install_carvel() {
@@ -31,6 +51,19 @@ install_yq() {
     yq --version
 }
 
+install_kp() {
+    log 'Installing kp'
+
+    get_latest_release "$REPO_KP" "linux-amd64"
+
+    while read -r line; do
+        wget -qO "$OUTPUT"/kp "$line"
+        chmod +x "$OUTPUT"/kp
+    done <<< "$DOWNLOAD_URL"
+
+    echo "kq cli:" $(kq version)
+}
+
 install_docker() {
     echo "installing docker"
     apt install docker.io -y
@@ -41,3 +74,4 @@ install_carvel
 install_kubectl
 install_yq
 install_docker
+install_kp
